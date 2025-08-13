b33a12652b117b55fd03c80018d75a0c0823dc3a
AhmadS7 <70293745+AhmadS7@users.noreply.github.com>
2025-08-13 23:00:04 +0600

    Update 2._Valid_Anagram.py

--- Phase_1:_Core_Foundations_(Arrays,_Hashing,_Two_Pointers)/2._Valid_Anagram.py
+++ Phase_1:_Core_Foundations_(Arrays,_Hashing,_Two_Pointers)/2._Valid_Anagram.py
@@ -1,3 +1,30 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
+
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s) != len(t):
+#             return False
+        
+#         sset = {}
+#         tset = {}
+
+#         for i in s:
+#             sset[i] = 1 + sset.get(i, 0)
+#         for i in t:
+#             tset[i] = 1 + tset.get(i, 0)
+#         return sset == tset
+
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s) != len(t):
+#             return False
+        
+#         start = ord('a')
+#         end = ord('z')
+    
+#         for i in range(start, end+1):
+#             if s.count(chr(i)) != t.count(chr(i)):
+#                 return False
+#         return True
