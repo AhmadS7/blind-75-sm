81c4c37defe63f1da10b72a3c62efb0b84bd35dc
AhmadS7 <70293745+AhmadS7@users.noreply.github.com>
2025-08-14 13:56:36 +0600

    Update 1:_Longest_Substring_Without_Repeating_Characters.py

--- Phase_2:_Sliding_Window_&_Dynamic_Programming/1:_Longest_Substring_Without_Repeating_Characters.py
+++ Phase_2:_Sliding_Window_&_Dynamic_Programming/1:_Longest_Substring_Without_Repeating_Characters.py
@@ -1,1 +1,15 @@
 
+def lengthOfLongestSubstring(s):
+    char_set = set()      # To store unique characters
+    left = 0              # Left pointer of the window
+    max_len = 0           # Result
+
+    for right in range(len(s)):  # Right pointer moves forward
+        while s[right] in char_set:  # If duplicate found
+            char_set.remove(s[left]) # Shrink window from left
+            left += 1
+        char_set.add(s[right])       # Add new character
+        max_len = max(max_len, right - left + 1)  # Update result
+    return max_len
+
+
